0e9238e25362ef680a7fd04113aafb67a967d008
Le Dinh Hieu <132204295+Ledinhhieu2212@users.noreply.github.com>
2023-09-19 20:01:32 +0700

    cap nhat connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$db_name = 'mysql:host=sql211.infinityfree.com;dbname=if0_35065408_food_db';
-$user_name = 'if0_35065408';
-$user_password = 'uGShaOEjtXZcK';
+$db_name = 'mysql:host=localhost;dbname=food_db';
+$user_name = 'root';
+$user_password = '';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
