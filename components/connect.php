0ffc804a794fb32c9e23c70644d18dab8be30f9f
Le Dinh Hieu <132204295+Ledinhhieu2212@users.noreply.github.com>
2023-09-19 19:55:19 +0700

    cap nhat ngay 19/09/2023

---
+++ components/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$db_name = 'mysql:host=sql211.infinityfree.com;dbname=if0_35065408_food_db';
+$user_name = 'if0_35065408';
+$user_password = 'uGShaOEjtXZcK';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+
+?>
